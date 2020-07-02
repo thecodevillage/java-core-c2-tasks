8c7744254eea72372293357fba3b60f868eab548
wamo-cha <isslewi03@gmail.com>
2020-07-02 20:32:05 +0300

    add code

--- Calculator.java
+++ Calculator.java
@@ -5,8 +5,9 @@ public static void main(String[] args){
   int a = 20;
   int sum = 0;
   int mwangemiNo = 17;
+  int lewisNo=20;
 
-  sum = a + mwangemiNo;
+  sum = a + mwangemiNo +lewisNo;
 
   System.out.println("Sum: "+sum);
 
