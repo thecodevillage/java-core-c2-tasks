37f6cbed55617dca02f78dc0f8fac7ba26921226
Visels <elviskipchumba@yahoo.com>
2020-07-02 20:01:57 +0300

    elvis variable

--- Calculator.java
+++ Calculator.java
@@ -4,8 +4,12 @@ public static void main(String[] args){
 
   int a = 20;
   int sum = 0;
+  int mwangemiNO = 17;
+  int elvisNo = 3;
+  sum = a + mwangemiNO + elvisNo;
 
-  sum = a;
+
+
 
   System.out.println("Sum: "+sum);
 
