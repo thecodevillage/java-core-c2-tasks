528868e0b028acb83c3de0f5ace5f14532c358e3
Niklaus <odingonicolus@gmail.com>
2020-07-02 20:03:24 +0300

    Added Odingo No

--- Calculator.java
+++ Calculator.java
@@ -3,9 +3,11 @@ public class Calculator {
 public static void main(String[] args){
 
   int a = 20;
+  int odingoNo = 20;
   int sum = 0;
 
-  sum = a;
+
+  sum = a + odingoNo;
 
   System.out.println("Sum: "+sum);
 
