eb9c3d3d0c2b612eb2d43cc2d5822287b9b695c5
Tamabut <atamabut@gmail.com>
2020-07-02 20:06:52 +0300

    amos

--- Calculator.java
+++ Calculator.java
@@ -3,11 +3,12 @@ public class Calculator {
 public static void main(String[] args){
 
   int a = 20;
-  int sum = 0;
+  int amos = 0;
 
-  sum = a;
 
-  System.out.println("Sum: "+sum);
+  amos = a;
+
+  System.out.println("Sum: "+amos);
 
 }
 
